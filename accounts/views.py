e106ce5da2bb6926cf310358285da96fe78edd7f
fandykun <fandy19061999@gmail.com>
2020-06-09 18:12:19 +0700

    tutorial 21-25

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
@@ -13,3 +14,23 @@ def signup_view(request):
         form = UserCreationForm()
 
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    print(request)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
